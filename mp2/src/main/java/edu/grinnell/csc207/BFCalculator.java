2d2433067bb8499c585de317c1827eca5a848177
yusufbon <yusufbonsen@gmail.com>
2024-12-20 15:58:45 -0600

    Update BFCalculator.java
    Fix style check errors: indentation, missing javadoc, and misuse of system.out.println

--- mp2/src/main/java/edu/grinnell/csc207/BFCalculator.java
+++ mp2/src/main/java/edu/grinnell/csc207/BFCalculator.java
@@ -1,71 +1,79 @@
+
 package edu.grinnell.csc207;
 
 /**
- * The calculator class that performs operations on BigFraction values
- * The calculator stores the last computed value and allows operations such as addition, subtraction, multiplication, and division.
- * It also allows the result to be reset to zero.
+ * The calculator class that performs operations on BigFraction values.
+ * The calculator stores the last computed value and allows operations such as
+ * addition, subtraction, multiplication, and division. It also allows the result
+ * to be reset to zero.
  *
  * @author Bonsen Yusuf
  */
 public class BFCalculator {
-    private BigFraction lastValue;
 
-    /*
-     * Constructs a new BFCalculator with initial value set to 0.
-     */
-    public BFCalculator() {
-        lastValue = new BigFraction(0, 1);
-    }
+  /**
+   * The last computed value stored as a BigFraction.
+   */
+  private BigFraction lastValue; // lastValue
 
-    /**
-     * Retrieves the last calculated value.
-     *
-     * @return the last computed fraction.
-     */
-    public BigFraction get() {
-        return lastValue;
-    }
+  /**
+   * Constructs a new BFCalculator with an initial value set to 0.
+   */
+  public BFCalculator() { // BFCalculator()
+    lastValue = new BigFraction(0, 1); // Initialize lastValue to 0/1
+  } // BFCalculator()
 
-    /**
-     * Adds a fraction to the last computed value.
-     *
-     * @param val the fraction being added
-     */
-    public void add(BigFraction val) {
-        lastValue = lastValue.add(val);
-    }
+  /**
+   * Retrieves the last calculated value.
+   *
+   * @return the last computed fraction.
+   */
+  public BigFraction get() { // get
+    return lastValue; // Return last computed value
+  } // get
 
-    /**
-     * Subtracts a fraction from the last computed value.
-     *
-     * @param val the fraction to subtract.
-     */
-    public void subtract(BigFraction val) {
-        lastValue = lastValue.subtract(val);
-    }
+  /**
+   * Adds a fraction to the last computed value.
+   *
+   * @param val the fraction being added.
+   */
+  public void add(BigFraction val) { // add
+    lastValue = lastValue.add(val); // Add value to last computed value
+  } // add
 
-    /**
-     * Multiplies the last computed value by a fraction.
-     *
-     * @param val the fraction to multiply by.
-     */
-    public void multiply(BigFraction val) {
-        lastValue = lastValue.multiply(val);
-    }
+  /**
+   * Subtracts a fraction from the last computed value.
+   *
+   * @param val the fraction to subtract.
+   */
+  public void subtract(BigFraction val) { // subtract
+    lastValue = lastValue.subtract(val); // Subtract value from last computed value
+  } // subtract
 
-    /**
-     * Divides the last computed value by a fraction.
-     *
-     * @param val the fraction to divide by.
-     */
-    public void divide(BigFraction val) {
-        lastValue = lastValue.divide(val);
-    }
+  /**
+   * Multiplies the last computed value by a fraction.
+   *
+   * @param val the fraction to multiply by.
+   */
+  public void multiply(BigFraction val) { // multiply
+    lastValue = lastValue.multiply(val); // Multiply last computed value by input value
+  } // multiply
 
-    /**
-     * Clears the last computed value, resetting it to 0.
-     */
-    public void clear() {
-        lastValue = new BigFraction(0, 1);
-    }
-}
+  /**
+   * Divides the last computed value by a fraction.
+   *
+   * @param val the fraction to divide by.
+   */
+  public void divide(BigFraction val) { // divide
+    lastValue = lastValue.divide(val); // Divide last computed value by input value
+  } // divide
+
+  /**
+   * Clears the last computed value, resetting it to 0.
+   */
+  public void clear() { // clear
+    lastValue = new BigFraction(0, 1); // Reset lastValue to 0/1
+  } // clear
+} // BFCalculator
+
+
